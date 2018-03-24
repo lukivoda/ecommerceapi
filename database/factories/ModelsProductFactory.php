d32ce935d17450b8044b8bcfdcb854a40885261f
lukivoda <stevanris@gmail.com>
2018-03-24 21:38:09 +0100

    seeding products and reviews with factory(faker library)

--- database/factories/ModelsProductFactory.php
+++ database/factories/ModelsProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' =>$faker->word,
+        'detail' =>$faker->paragraph,
+        'stock' =>$faker->randomDigit,
+        'discount' =>$faker->numberBetween(2,50),
+        'price' =>$faker->numberBetween(100,1000),
     ];
 });
--- database/factories/ModelsReviewFactory.php
+++ database/factories/ModelsReviewFactory.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
-        //
+
+        'product_id' => function(){
+        //give us random number from the number of the columns of the table
+        return Product::all()->random();
+
+        },
+        'customer' =>$faker->name,
+        'review' =>$faker->paragraph,
+        'star' =>$faker->numberBetween(0,5),
+
     ];
 });
