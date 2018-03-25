b0e8d76870892995eaab79a7f38b20dad600a5d8
lukivoda <stevanris@gmail.com>
2018-03-25 21:03:59 +0200

    modifying product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,18 @@ class ProductResource extends Resource
           'name' => $this->name,
           'description' =>$this->detail,
            'price' =>$this->price,
-            'stock' =>$this->stock,
-           'discount' =>$this->discount
+            //if number of products is 0 we print Out of stock
+            'stock' =>$this->stock == 0?'Out of stock':$this->stock,
+           'discount' =>$this->discount,
+               //roundind the number that we are  getting with price - discount price
+            'total_price' => $this->discount>0?round($this->price-($this->discount/100*$this->price),2):$this->price,
+
+            //checking if we have reviews(if we have we divide sum of stars with number of reviews and then round that number)
+
+            'rating' => $this->reviews->count()>0?round($this->reviews->sum('star')/$this->reviews->count()):"No reviews yet.",
+            'href' => [
+                "reviews" => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
